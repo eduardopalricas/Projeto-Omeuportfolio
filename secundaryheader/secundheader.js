19f0cf1a167cec107e514a80441205cb089e5572
Eduardo Palricas Cardoso <99208121+eduardopalricas@users.noreply.github.com>
2022-07-11 20:56:49 +0100

    Finishing secundary pages

---
+++ secundaryheader/secundheader.js
@@ -0,0 +1,93 @@
+class SecundaryHeader extends HTMLElement {
+  constructor(){
+    super()
+
+    const shadow = this.attachShadow({mode: 'open'})
+
+    shadow.innerHTML = `
+    <header> 
+    <h1 id="name"><a href="../index.html"><img src="../images/arrowleft.png" alt="Imagem de uma seta para a esquerda" id="arrowleft"></a> Eduardo Cardoso</h1>
+    <iframe src="../Socialmedia/socialmedia.html" frameborder="0" id="socialmedia"></iframe>     
+  </header>
+  `
+  const style = document.createElement('style')
+  style.textContent = `
+  @import url('https://fonts.googleapis.com/css2?family=Comforter&family=Lobster&family=Luckiest+Guy&family=Passion+One:wght@400;700;900&family=Playfair+Display&family=Quintessential&family=Roboto:ital,wght@0,400;1,700&family=Sriracha&display=swap');
+
+
+  @import url('https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@1,400;1,700&family=Comforter&family=Lobster&family=Luckiest+Guy&family=Passion+One:wght@400;700;900&family=Playfair+Display&family=Quintessential&family=Roboto:ital,wght@0,400;1,700&family=Sriracha&display=swap');
+  
+  
+  @import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,400;1,300&family=Alegreya:ital,wght@1,400;1,700&family=Comforter&family=Lobster&family=Luckiest+Guy&family=Passion+One:wght@400;700;900&family=Playfair+Display&family=Quintessential&family=Roboto:ital,wght@0,400;1,700&family=Sriracha&display=swap');
+  
+  :root{
+    --corfundo: #ABC7FF;
+    --corfundodestaque: #6891e2;
+    --cortitulo: #0B2B6B;
+    --corsubtitulo: #4d69a1;
+    --letranome: Luckiest Guy;
+    --letratitulos: Alegreya;
+    --letratexto: Alegreya Sans;
+  }
+  
+  
+  body,html{
+    box-sizing: content-box;
+    margin: 0px;
+    padding: 0px;
+  }
+  
+  
+  body{
+    height: 15vh;
+  }
+  
+  
+  header{
+    height: 15vh;
+    background-color: var(--corfundodestaque);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 2px 2px 4px #6891e27c;
+  }
+  
+  
+  #arrowleft{
+    width: 40px;
+    height: 40px;
+    margin-left:calc(2vw + 10px);
+    margin-right:calc(2vw + 10px);
+  }
+  
+  
+  h1{
+    display: flex;
+    align-items: center;
+  }
+  
+  a{
+    height: 15vh;
+    display: flex;
+    align-items: center;
+  }
+  
+  
+  #socialmedia{
+    height: 15vh;
+    margin: 0px calc(2vw + 10px);
+    width: 250px;
+  }
+  
+  
+  #name{
+    font-family: var(--letranome);
+    color: var(--cortitulo);
+    font-size: 40px;
+  }`
+
+  shadow.appendChild(style)      
+  }
+}
+
+customElements.define('my-header', SecundaryHeader)
